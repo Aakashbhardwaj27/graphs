af3e17572c540b3591247949f5016b7837ad1a34
Aakash Bhardwaj <officialaakashbhardwaj@gmail.com>
2022-08-12 00:53:07 +0530

    added graph embedd

--- src/App.js
+++ src/App.js
@@ -1,23 +1,74 @@
-import logo from './logo.svg';
+/* eslint-disable jsx-a11y/iframe-has-title */
+import React from 'react';
 import './App.css';
+import { embedDashboard } from "@superset-ui/embedded-sdk";
+import axios from 'axios';
+import { useEffect } from 'react';
 
 function App() {
+
+
+
+  const init = async () => {
+     
+  const dashboardID = "759847f3-4564-48f4-8ac6-5488ac87620c";
+  const superset_url = 'http://localhost:8088';
+  const AdminToken = () => {
+    const response= axios.post(`${superset_url}/api/v1/security/login/`, {
+      password:"admin",
+      username:"admin",
+      provider:"db",
+      refresh:true
+    })
+
+    return response.access_token
+  }
+
+
+  const adminToken = await AdminToken();
+  
+  const bodyParameters={
+    user: {
+      username: "myAppUser",
+      first_name: "MyApp User",
+      last_name: "MyApp User"
+    }, resources: [{ type: "dashboard", id:dashboardID  }], rls: [{ clause: "customer_id=4" }]
+  }
+  
+
+  const config = {
+    headers: {
+      'Authorization': `Bearer ${adminToken}`
+    }
+  };
+
+
+  const fetchGuestTokenFromBackend = async (config) => {
+   const response= axios.post(`${superset_url}/api/v1/security/guest_token/`,bodyParameters,config)
+
+   return response.token
+  }
+    const token =await fetchGuestTokenFromBackend(config);
+  
+    embedDashboard({
+    id: dashboardID,  // given by the Superset embedding UI
+    supersetDomain: "https://localhost:8088",
+    mountPoint: document.getElementById("container"), // html element in which iframe render
+    fetchGuestToken: () => token,
+    dashboardUiConfig: { hideTitle: true }
+   })
+  }
+  
+  useEffect(() => {
+   
+   init()
+   }, [])
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+  <h1>Superset Embedding</h1>
+      <div id="container"></div>
+     
     </div>
   );
 }
